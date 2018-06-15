5652d6f4bf4bdabf6dbbc60977b63cd1240ef043
pacoj09 <pacoalvarado0923@gmail.com>
2018-06-14 21:02:55 -0600

    Proyecto 1.0

---
+++ Clases/clsEsquema.cs
@@ -0,0 +1,45 @@
+﻿using ConexionBD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+   public class clsEsquema
+    {
+        public List<string> ListaTablas { get; set; }
+
+        public clsEsquema() { }
+
+        public bool CargarEsquema()
+        {
+            ListaTablas = new List<string>();
+            bool exito = false;
+            clsConexion objConexion = new clsConexion();
+            if (objConexion.abrirConexion())
+            {
+                DataTable dtTablas = new DataTable();
+                string query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE';";
+                dtTablas = objConexion.consultar(query);
+                if (dtTablas != null)
+                {
+                    for (int i = 0; i < dtTablas.Rows.Count; i++)
+                    {
+                        ListaTablas.Add(dtTablas.Rows[i][0].ToString());
+                    }
+                    exito = true;
+                }
+            }
+            return exito;
+        }
+
+
+        ~clsEsquema()
+        {
+            ListaTablas = null;
+        }
+    }
+}
---
+++ Clases/clsGenerador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class clsGenerador
+    {
+        public string linew { get; set; }
+        //string linew;
+        string liner;
+
+        public void GenerateMethod()
+        {
+            ///Este metodo debe de generar el string completo con el codigo del Metodo
+            linew = "El codigo generado";
+        }
+
+        public void WriteMethod()
+        {
+            StreamReader sr = new StreamReader("C:\\Test\\clsPrueba.cs");
+            liner = sr.ReadLine();
+
+            while (liner != null)
+            {
+                if (liner.Equals("#region View"))
+                {
+                    long lineposition = sr.BaseStream.Position + 1;
+                    sr.Close();
+                    StreamWriter sw = new StreamWriter("C:\\Test\\clsPrueba.cs");
+                    sw.BaseStream.Position = lineposition;
+                    sw.WriteLine(linew);
+                    //while (liner.Equals("#endregion View"))
+                    //{
+                    //    linew = "";
+                    //    sw.WriteLine(linew);
+                    //    liner = sr.ReadLine();
+                    //}
+                    //linew = "} \n #endregion View";
+                    //sw.WriteLine(linew);
+                }
+                liner = sr.ReadLine();
+            }
+        }
+    }
+}
